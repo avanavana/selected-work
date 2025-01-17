dd136380853c85d56155b6e5083e568b1d34d356
Avana Vana <dear.avana@gmail.com>
2025-01-16 21:38:59 -0500

    chore: installed million lint for vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
+import MillionLint from '@million/lint'
 import path from "path"
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths()],
+  plugins: [ react(), svgr(), tsconfigPaths(), MillionLint.vite() ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
